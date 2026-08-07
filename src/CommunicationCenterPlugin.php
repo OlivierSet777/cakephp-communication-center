7869b2956fc84ca6a39c075b8aec800708a07e3a
olivier Set <pusset82@hotmail.fr>
2026-08-07 14:55:30 +0200

    feat: add Communication Center plugin class

---
+++ src/CommunicationCenterPlugin.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace CommunicationCenter;
+
+use Cake\Core\BasePlugin;
+
+/**
+ * Plugin for Communication Center.
+ */
+class CommunicationCenterPlugin extends BasePlugin
+{
+}
