a70bbbb645eab62fb1901639e1ebdb14e6842821
olivier Set <pusset82@hotmail.fr>
2026-08-14 20:24:03 +0200

    fix: wrap email content for table based layouts

--- templates/email/html/default.php
+++ templates/email/html/default.php
@@ -4,4 +4,37 @@
  */
 ?>
 
-<?= nl2br(h($message)) ?>
+<tr>
+    <td class="w640" width="640" bgcolor="#FFFFFF">
+        <table
+            class="w640"
+            width="640"
+            cellpadding="0"
+            cellspacing="0"
+            border="0"
+        >
+            <tbody>
+                <tr>
+                    <td class="w30" width="30"></td>
+
+                    <td
+                        class="w580"
+                        width="580"
+                        valign="top"
+                        style="
+                            padding-top:30px;
+                            padding-bottom:30px;
+                            font-size:16px;
+                            line-height:1.6;
+                            color:#333333;
+                        "
+                    >
+                        <?= nl2br(h($message)) ?>
+                    </td>
+
+                    <td class="w30" width="30"></td>
+                </tr>
+            </tbody>
+        </table>
+    </td>
+</tr>
